Fix Tensor broadcasting

test/src/ggml/Tensor/rsqrt.py
@@ -3,10 +3,10 @@ import torch
 
 class TestTensorRsqrt(TestCase):
     def test_rsqrt_1d(self):
-        data = 4.0
+        data = [4.0]
         pt = torch.tensor(data)
         expected = torch.rsqrt(pt)
-        actual = self.cli('rsqrt', '--this', '[' + str(pt.tolist()) + ']')
+        actual = self.cli('rsqrt', '--this', str(pt.tolist()))
         self.assertTensors(actual, [expected])
     def test_rsqrt_2d(self):
         data = [[4.0], [25.0]]
4f21b70a6cb1017bdcded4c7d06a3aced1042e16
oinuar <jussi.raunio@gmail.com>
2026-09-06 09:00:58 +0300

    Fix unit tests.

--- src/ggml/DeviceAllocator.cpp
+++ src/ggml/DeviceAllocator.cpp
@@ -4,15 +4,18 @@
 #include "ggml/Context.hpp"
 
 DeviceAllocator::DeviceAllocator(Context& context, const Device& device)
-    : context_(context), buft_(ggml_backend_dev_buffer_type(*device)), buffer_(std::nullopt)
+    : context_(context), buft_(ggml_backend_dev_buffer_type(*device)), buffers_()
 {
 }
 
 void DeviceAllocator::allocate(const std::optional<ggml_backend_buffer_usage>& usage) {
-    if (buffer_)
-        throw new std::runtime_error("Already allocated");
-
     auto buffer = ggml_backend_alloc_ctx_tensors_from_buft(*context_, buft_);
 
-    buffer_.emplace(buffer, usage);
+    // NULL is returned when every tensor in the context already has a buffer,
+    // e.g. when the graph is built entirely in another context. There is
+    // nothing to allocate in that case.
+    if (buffer == nullptr)
+        return;
+
+    buffers_.emplace_back(buffer, usage);
 }
